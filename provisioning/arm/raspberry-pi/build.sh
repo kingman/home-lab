1e3c798bbf4a6ca041574cf5ad316010b7c570e7
Charlie Wang <charlie.zwang@gmail.com>
2020-10-19 11:41:40 +0200

    lint issue

--- provisioning/arm/raspberry-pi/build.sh
+++ provisioning/arm/raspberry-pi/build.sh
@@ -6,8 +6,6 @@ git clone --depth=1 https://github.com/raspberrypi/linux
 
 cd linux
 
-KERNEL=kernel8
-
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+KERNEL=kernel8; make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
 
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
