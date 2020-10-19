e841453b7204a976b428b991d22bb6f1a4442d3d
Charlie Wang <charlie.zwang@gmail.com>
2020-10-19 10:13:36 +0200

    fix lint issue

---
+++ provisioning/arm/raspberry-pi/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e
+
+git clone --depth=1 https://github.com/raspberrypi/linux
+
+cd linux
+
+KERNEL=kernel8
+
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
